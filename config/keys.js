e4f6996a48ea858416a26849423a6d3c9a772fb4
prabhudev Vatnal <prabhudev.vatnal@global.loc>
2019-07-17 16:22:18 +0530

    DB connected to Online

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports ={
-    mongoURI : 'mongodb://127.0.0.1:27017/imdb'
+    mongoURI : 'mongodb+srv://prabhudevv:prabhudevv@pvcluster0-ocegn.mongodb.net/imdb?retryWrites=true&w=majority'
 }
